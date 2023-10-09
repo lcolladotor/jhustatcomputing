62662718b92deb5f6822625e2403d09a75ed5a87
lcolladotor <lcolladotor@gmail.com>
2023-10-09 12:45:56 -0400

    Fix typo + test that all 3 data files exist

--- projects/project-3/index.R
+++ projects/project-3/index.R
@@ -1,70 +1,73 @@
-
+## ----eval=FALSE------------------------------------
 ## b_lyrics <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-29/beyonce_lyrics.csv")
 ## ts_lyrics <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-29/taylor_swift_lyrics.csv")
 ## sales <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-29/sales.csv")
 
 
-
-library(here)
-if (!file.exists(here("data", "b_lyrics.RDS"))) {
+## ----message=FALSE---------------------------------
+library("here")
+rds_files <- c("b_lyrics.RDS", "ts_lyrics.RDS", "sales.RDS")
+## Check whether we have all 3 files
+if (any(!file.exists(here("data", rds_files)))) {
+    ## If we don't, then download the data
     b_lyrics <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-29/beyonce_lyrics.csv")
     ts_lyrics <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-29/taylor_swift_lyrics.csv")
     sales <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-29/sales.csv")
 
-    # save the files to RDS objects
+    ## Then save the data objects to RDS files
     saveRDS(b_lyrics, file = here("data", "b_lyrics.RDS"))
     saveRDS(ts_lyrics, file = here("data", "ts_lyrics.RDS"))
     saveRDS(sales, file = here("data", "sales.RDS"))
 }
 
 
-
+## ----eval=TRUE, message=FALSE----------------------
 b_lyrics <- readRDS(here("data", "b_lyrics.RDS"))
 ts_lyrics <- readRDS(here("data", "ts_lyrics.RDS"))
 sales <- readRDS(here("data", "sales.RDS"))
 
 
-
+## --------------------------------------------------
 # Add your solution here
 
 
-
+## --------------------------------------------------
 # Add your solution here
 
 
-
+## --------------------------------------------------
 # Add your solution here
 
 
-
+## --------------------------------------------------
 # Add your solution here
 
 
-
+## --------------------------------------------------
 # Add your solution here
 
 
-
+## --------------------------------------------------
 # Add your solution here
 
 
-
+## --------------------------------------------------
 # Add your solution here
 
 
-
+## --------------------------------------------------
 # Add your solution here
 
 
-
+## --------------------------------------------------
 # Add your solution here
 
 
-
+## --------------------------------------------------
 # Add your solution here
 
 
-
+## --------------------------------------------------
 options(width = 120)
 sessioninfo::session_info()
 
