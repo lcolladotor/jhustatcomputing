621522c6151cdb6b8232ee7638717761b17c29ad
lcolladotor <lcolladotor@gmail.com>
2024-08-28 23:13:42 -0400

    re-ran scripts/auto_style.R and scripts/tangle.R

--- posts/03-introduction-to-gitgithub/index.R
+++ posts/03-introduction-to-gitgithub/index.R
@@ -1,4 +1,4 @@
-## --------------------------------------------------------------------------------
+## ----------------------------------------------------------------------------
 #| label: versioncontrol
 #| echo: false
 #| fig-cap: 'How not to use GitHub [image from PhD Comics]'
@@ -6,7 +6,7 @@
 knitr::include_graphics("../../images/phdversioncontrol.gif")
 
 
-## --------------------------------------------------------------------------------
+## ----------------------------------------------------------------------------
 #| label: happygitwithr
 #| echo: false
 #| fig-cap: 'A screenshot of the Happy Git with R online book from Jenny Bryan'
@@ -14,12 +14,12 @@ knitr::include_graphics("../../images/phdversioncontrol.gif")
 knitr::include_graphics("../../images/happygitwithr.png")
 
 
-## --------------------------------------------------------------------------------
+## ----------------------------------------------------------------------------
 #| eval: false
 ## install.packages("reprex")
 
 
-## --------------------------------------------------------------------------------
+## ----------------------------------------------------------------------------
 options(width = 120)
 sessioninfo::session_info()
 
