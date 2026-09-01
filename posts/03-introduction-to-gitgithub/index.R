32d3b86e97f64c8b40323e36d8797c4b4e9d8287
lcolladotor <lcolladotor@gmail.com>
2026-09-01 08:42:20 -0400

    Re-ran tangle.R

--- posts/03-introduction-to-gitgithub/index.R
+++ posts/03-introduction-to-gitgithub/index.R
@@ -1,4 +1,4 @@
-## -------------------------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------------------
 #| label: versioncontrol
 #| echo: false
 #| fig-cap: 'How not to use GitHub [image from PhD Comics]'
@@ -6,7 +6,7 @@
 knitr::include_graphics("../../images/phdversioncontrol.gif")
 
 
-## -------------------------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------------------
 #| label: happygitwithr
 #| echo: false
 #| fig-cap: 'A screenshot of the Happy Git with R online book from Jenny Bryan'
@@ -14,7 +14,7 @@ knitr::include_graphics("../../images/phdversioncontrol.gif")
 knitr::include_graphics("../../images/happygitwithr.png")
 
 
-## -------------------------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------------------
 #| eval: false
 # ## Install "usethis" if you don't have it already
 # if (!requireNamespace("usethis", quietly = TRUE)) {
@@ -23,17 +23,17 @@ knitr::include_graphics("../../images/happygitwithr.png")
 # 
 # usethis::create_project("~/Desktop/biostat776classnotes")
 # 
-# ## Then in your new RStudio project, run the following:
+# ## Then in your new Positron window / RStudio project, run the following:
 # usethis::use_git()
 # usethis::use_github()
 
 
-## -------------------------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------------------
 #| eval: false
 # install.packages("reprex")
 
 
-## -------------------------------------------------------------------------------------------------------------------------------------------
+## -----------------------------------------------------------------------------------------
 options(width = 120)
 sessioninfo::session_info()
 
